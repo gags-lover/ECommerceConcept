a56dc3f99e75bf328a3352a36a6a8a4fc73b1330
Dinar Taziev <astat1c24@gmail.com>
2022-06-27 22:42:36 +0300

    initial commit

---
+++ app/src/main/java/com/github/astat1cc/sergeybalakintesttask/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.github.astat1cc.sergeybalakintesttask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.astat1cc.sergeybalakintesttask.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/github/astat1cc/sergeybalakintesttask/SplashFragment.kt
@@ -0,0 +1,36 @@
+package com.github.astat1cc.sergeybalakintesttask
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.github.astat1cc.sergeybalakintesttask.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (view.isVisible) {
+                findNavController().navigate(R.id.mainFragment)
+            }
+        }, 1500)
+    }
+}
